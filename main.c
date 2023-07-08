9165863b2b07c828352fe2994529e929a914382f
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-08 13:50:00 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+// Program to categorize a single character that is entered at the terminal
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  char c;
+
+  printf ("Enter a single character: ");
+  scanf ("%c", &c);
+
+  if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+    printf ("It is an alphabetic character.\n");
+  }
+  else if ( c >= '0' && c <= '9' ) {
+    printf ("It is a digit.\n");
+  }
+  else {
+    printf ("It's a special character.\n");
+  }
+  
   return 0;
 }
